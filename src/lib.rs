bc9702fb3ce0a95999e958f87e9b7d2970763426
dmitri9149 <dmitri.as.name@gmail.com>
2021-04-04 12:07:10 +0300

    type aliases moved to lib

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,11 @@ pub use crate::word_vocabs::{Vocab,Qxx,Ixx};
 pub use tokens_vocab::{VocabOfTokens};
 pub use token_merges::{CandidatesForMerge};
 
+// indexation of tokens 
+pub type Ind = usize;
+// for tokens quantity
+pub type Quant = u32;
+
 
 pub fn replace_chars_to_char(input:&str, aa:&str, b:char) -> String {
     let mut output = String::with_capacity(input.len());
