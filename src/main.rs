11666f450d4c5af062ffbc128cd27fad7b47a79a
dmitri9149 <dmitri.as.name@gmail.com>
2021-04-09 15:58:50 +0300

    do not eliminate some chars

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@ use translationlib::*;
 
 fn main() {
     let mut txt = CorpusAsString::corpus_from_file("data/fra_eng/fra.txt");
+/*
 // eliminate u{202f} non breaking space and '\xa0' which in unicode is u{00a0}
     txt = txt.replace_some_chars("\u{202f}\u{00a0}", ' ');
+*/
     txt = txt.replace_some_chars("\"",' ');
     txt.separate_punctuation("?!.,");
 
     let sentences = SentencesForTranslation::from_corpus(&txt);
 
     println!("The first part of the text {:?}\n", &txt.processed[0..1000]);
+    println!("The number of pairs for translation: {:?}\n", sentences.size);
     println!("The first pairs for translation {:?}\n", &sentences.eng[0..10]);
     for i in 0..1000 {
         println!("{:?}         {:?}", sentences.eng[i], sentences.fra[i]); 
