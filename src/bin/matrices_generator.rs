c87c7caa57939c85b2451f11577c88959f97c2a9
dmitri9149 <dmitri.as.name@gmail.com>
2021-04-16 13:03:55 +0300

    initial commit

---
+++ src/bin/matrices_generator.rs
@@ -0,0 +1,15 @@
+// take data from tokens_generator (sentencesAsIndicesDynamics) which are in 
+// the form of sentence -> (token_indices) like [1,56,390] where the nubers are 
+// indices for initial and newly generated tokens
+// the data are transformed to the Matrix to be used for matrix transformations in 
+// machine translation 
+
+//use serde_json::{Result, Value};
+
+use std::fs::File;
+
+fn main() -> Result<(),Box<dyn std::error::Error>>{
+
+    Ok(())
+}
+
