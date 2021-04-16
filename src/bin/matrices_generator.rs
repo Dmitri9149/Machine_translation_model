7060ea3080eaddd2462f156d7f2544bc90e4c545
dmitri9149 <dmitri.as.name@gmail.com>
2021-04-16 14:18:57 +0300

    initial functionality for reading serde from file

--- src/bin/matrices_generator.rs
+++ src/bin/matrices_generator.rs
@@ -5,11 +5,30 @@
 // machine translation 
 
 //use serde_json::{Result, Value};
-
+use translationlib::*;
 use std::fs::File;
+use std::time::Instant;
+use std::fs::read_to_string; // use instead of std::fs::File
+use std::path::Path;
 
+
+
+/*
 fn main() -> Result<(),Box<dyn std::error::Error>>{
 
     Ok(())
+}
+*/
+
+// started development from here 
+// https://github.com/serde-rs/serde/issues/1195
+fn main() {
+    let json_file_path = Path::new("data/data.json");
+    let json_file_str = read_to_string(json_file_path).expect("file not found");
+    let start = Instant::now();
+    // use instead of from_reader
+    let sentences:SentencesAsIndicesDynamicsLang = serde_json::from_str(&json_file_str).expect("error while reading json");
+    let elapsed = start.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
 }
 
