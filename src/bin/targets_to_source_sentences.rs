79e6b8c6edcc5c5f329257e89ced596e4739fa59
dmitri9149 <dmitri.as.name@gmail.com>
2021-05-10 08:56:34 +0300

    bug is fixed

--- src/bin/targets_to_source_sentences.rs
+++ src/bin/targets_to_source_sentences.rs
@@ -23,7 +23,8 @@ use std::fs::read_to_string;
 
 use translationlib::{NOWORD
     ,word_dynamics::dynamics::SentencesAsIndicesDynamicsN
-    ,TARGET_SENTENCE_MAX_LEN};
+    ,TARGET_SENTENCE_MAX_LEN
+    ,SOURCE_SENTENCE_MAX_LEN};
 
 use translationlib::targets_to_sentences::targets_to_lengths::{TargetsPosition
     ,TargetWordsToSentences
@@ -60,7 +61,7 @@ fn main()  -> Result<(),Box<dyn std::error::Error>> {
 
     let mut target_words_to_sentence_lengths = TargetWordsToSentenceLengths
         ::from_words_to_sentences(&target_words_to_sentences,&sentences);
-    target_words_to_sentence_lengths.lengths_likelihood(&TARGET_SENTENCE_MAX_LEN);
+    target_words_to_sentence_lengths.lengths_likelihood(&SOURCE_SENTENCE_MAX_LEN);
 
     let target_words_count = PositionalTargetWordsCount::new()
         .from_target_words_to_sentence_lengths(&target_words_to_sentence_lengths);
